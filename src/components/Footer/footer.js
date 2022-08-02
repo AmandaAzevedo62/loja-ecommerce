ecd1ea2b01780bb93e4b58eab8742a29fa8eb085
AmandaAzevedo62 <amandaazevedo62@gmail.com>
2022-08-02 18:23:28 -0300

    Alterações no Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,20 +1,69 @@
 import React from "react";
-import { FooterPai } from './styled'
+import { FooterPai , Newsletter , MainFooter, Titulo } from './styled'
 import facebook from '../img/facebook.png'
 import insta from '../img/insta.png'
 
 function Footer() {
     return (
         <FooterPai>
-            <span>
-                <h3>Siga-nos nas redes sociais</h3>
-            </span>
-            <a href="https://www.facebook.com/" target="_blank">
-                <img src={facebook} />
-            </a>
-            <a href="https://www.instagram.com/" target="_blank">
-                <img src={insta} />
-            </a>
+            <Newsletter>
+                <div>
+                    <h1>Cadastre-se e fique por dentro das novidades</h1>
+                </div>
+            <form action="/pagina-processa-dados-do-form" method="post">
+                <span>                    
+                    <input type="text" id="name" placeholder="Digite seu nome" />
+                </span>
+                <span>                    
+                    <input type="email" id="mail" placeholder="Digite seu email"/>
+                </span>
+                <span class="button">
+                    <button type="submit">Enviar sua mensagem</button>
+                </span>
+</form>
+            </Newsletter>
+            <MainFooter>
+            <div>
+                <div>
+                <div>
+                  <table>
+                  <Titulo>Menu</Titulo>
+                  <tr><a href="#">Quem somos</a></tr>
+                  <tr><a href="#">Nossos produtos</a></tr>
+                  <tr><a href="#">Contatos</a></tr>
+                  </table>
+              </div>
+
+              <div>
+                  <table>
+                  <Titulo>Menu</Titulo>
+                  <tr><a href="#">Quem somos</a></tr>
+                  <tr><a href="#">Nossos produtos</a></tr>
+                  <tr><a href="#">Contatos</a></tr>
+                  </table>
+              </div>
+
+              <div>
+                  <table>
+                  <Titulo>Menu</Titulo>
+                  <tr><a href="#">Quem somos</a></tr>
+                  <tr><a href="#">Nossos produtos</a></tr>
+                  <tr><a href="#">Contatos</a></tr>
+                  </table>
+              </div>
+
+              <div>
+                  <h4>Nossos contatos</h4>
+                  <div class="social">
+                      <a href="https://pt-br.facebook.com/login/" target="_blank"><i class='bx bxl-facebook'></i></a>
+                      <a href="https://www.instagram.com/accounts/login/?hl=pt-br" target="_blank"><i
+                              class='bx bxl-instagram'></i></a>
+                      <a href="https://twitter.com/" target="_blank"><i class='bx bxl-twitter'></i></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+            </MainFooter>
         </FooterPai>
     )
 }
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -3,15 +3,77 @@ import styled from "styled-components"
 export const FooterPai = styled.footer`
     display: flex;
     align-items: center;
+    flex-direction: column;
     height: 100%;
     justify-content: center;
-    background-color: #4B535E;
-    color: #016ABC;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    font-weight: bold;
+    color: #2d3a21;
+    font-family: 'Arvo', serif;
     gap: 7px;
+    margin-top: 4rem;
+    margin-left: 2rem;
 
     img{
         height: 35px;
     }
+`
+export const Newsletter = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #efefef;
+    width: 80%;
+    height: 200px;
+    color: #2d3a21;
+
+    h1 {
+        padding-bottom: 1rem;
+    }
+
+    span {
+    padding-left: 2rem;
+    }
+
+    input {
+    padding: 1rem;
+    width: 250px;
+    border: 1px solid grey;
+    border-radius: 5px;
+    }
+
+    button {
+    padding: 1rem;
+    width: 200px;
+    border: 1px solid grey;
+    border-radius: 25px;
+    background-color: #2d3a21;
+    color:#fff;
+    }
+
+    button:hover {
+        background-color: #fff;
+        color: #2d3a21;
+        cursor: pointer;
+    }
+`
+
+export const MainFooter = styled.footer`
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    text-align: left;
+    padding: 2rem;
+    margin-left: 2rem;
+
+    div {
+        width: 100%;
+        display: flex;
+        
+    }
+`
+
+export const Titulo = styled.tr`
+    color: #2d3a21;
+    font-family: 'Arvo', serif;
+    font-weight: bold;
 `
